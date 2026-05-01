f1c3621a9898ad611a08d24685885eaa75a05f71
muskansinghal12 <64657511+muskansinghal12@users.noreply.github.com>
2026-05-01 14:37:45 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subsetsHelper(self,nums,i,current_set,ans):
+        if i == len(nums):
+            # new_ans = []
+            # new_ans.extend(current_set)
+            ans.append(current_set[:])
+            return
+            # current_set = []
+        current_set.append(nums[i])
+        self.subsetsHelper(nums,i+1,current_set,ans)
+        current_set.pop()
+        self.subsetsHelper(nums,i+1,current_set,ans)
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        self.subsetsHelper(nums,0,[],ans)
+        return ans
+        
