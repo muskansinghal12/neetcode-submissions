d58e1cae8a3340c37a23c0b4548f3b40d0a99510
muskansinghal12 <64657511+muskansinghal12@users.noreply.github.com>
2026-05-04 23:31:43 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return n
+        ans = [0]*(n+1)
+        # print(ans)
+        ans[0] = 1
+        ans[1] = 1
+        for i in range(2,n+1):
+            ans[i] = ans[i-1] + ans[i-2]
+        # print(ans)
+        return ans[n]
+        
