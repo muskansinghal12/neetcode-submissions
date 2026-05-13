6c707eebc9c527e09b12399814b0e347a993d1e1
muskansinghal12 <64657511+muskansinghal12@users.noreply.github.com>
2026-05-13 22:55:33 +0530

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mapping = {}
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            if sorted_word not in mapping:
+                mapping[sorted_word] = []
+            mapping[sorted_word].append(word)
+        # print(mapping.values())
+        return list(mapping.values())
+        
